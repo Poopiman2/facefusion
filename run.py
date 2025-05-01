0be3628521b03c129b51fa5983e1c3dca50bb795
Poopiman2 <poopiman2342@gmail.com>
2025-05-01 02:14:32 -0400

    Update run.py

--- run.py
+++ run.py
@@ -1,16 +1,2 @@
-import argparse
-from facefusion import core
-from facefusion import state_manager
-
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--execution-providers', type=str, default='cpu', help='Execution provider: cuda, cpu, etc.')
-    args = parser.parse_args()
-
-    # Save it to state_manager so core/other modules can access it
-    state_manager.set_item('execution_provider', args.execution_providers)
-
-    core.launch()
-
-if __name__ == '__main__':
-    main()
+from facefusion.core import launch
+launch()
